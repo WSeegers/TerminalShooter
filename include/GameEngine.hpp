1b4950c1791009014bbe86025af2c36dd0de9985
Werner SEEGERS <wseegers@c3r2s4.wethinkcode.co.za>
2019-06-08 20:17:33 +0200

    added entity ot body

--- include/GameEngine.hpp
+++ include/GameEngine.hpp
@@ -22,7 +22,7 @@ class GameEngine
 public:
 	static const int MIN_HEIGHT = 25;
 	static const int MIN_WIDTH = 25;
-	static const int FRAME_RATE = 60;
+	static const int FRAME_RATE = 100;
 	static const timespec frameTime; // = {0, SEC(1) / FRAME_RATE};
 
 	GameEngine(void);
