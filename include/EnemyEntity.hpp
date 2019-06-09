b38fa70993e433f9c520ddd425b882e240a8fe42
Gj STEYN <gsteyn@c3r2s6.wethinkcode.co.za>
2019-06-09 02:08:01 +0200

    Give EnemyEntity age

--- include/EnemyEntity.hpp
+++ include/EnemyEntity.hpp
@@ -10,11 +10,17 @@ class EnemyEntity : public Body, public IKillable, public Mob
 public:
 	EnemyEntity(const Vec2 position, const Body &body);
 	EnemyEntity(const Body &body);
+	EnemyEntity(const Vec2 position, const Body &body, void (*update)(EnemyEntity &));
 
 	void update();
 
+	uint32_t getAge();
+	void setAge(uint32_t age);
+
 private:
 	EnemyEntity();
+	void (*_update)(EnemyEntity &self);
+	uint32_t _age;
 };
 
 #endif
--- src/EnemyEntity.cpp
+++ src/EnemyEntity.cpp
@@ -6,7 +6,25 @@ EnemyEntity::EnemyEntity(const Vec2 position, const Body &body)
 EnemyEntity::EnemyEntity(const Body &body)
 		: Entity(), Body(body) {}
 
+EnemyEntity::EnemyEntity(const Vec2 position, const Body &body, void (*update)(EnemyEntity &))
+		: Entity(position), Body(body), _update(update) {}
+
 void EnemyEntity::update()
 {
-	this->_position += _velocity;
+	this->_age++;
+
+	if (this->_update)
+	{
+		this->_update(*this);
+	}
+}
+
+uint32_t EnemyEntity::getAge()
+{
+	return (this->_age);
+}
+
+void EnemyEntity::setAge(uint32_t age)
+{
+	this->_age = age;
 }
