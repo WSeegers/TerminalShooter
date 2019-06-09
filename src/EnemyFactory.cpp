51f9921c5726c4dedbab1e5b90a8b33e12c0197f
Gj STEYN <gsteyn@c3r2s6.wethinkcode.co.za>
2019-06-09 02:08:56 +0200

    Give basic enemy simple behaviour

--- src/EnemyFactory.cpp
+++ src/EnemyFactory.cpp
@@ -1,6 +1,12 @@
 #include "EnemyFactory.hpp"
 
 Body basic("*   * ***   *  ", 5, 3);
+void basicBehavior(EnemyEntity &self)
+{
+	self.setVelocity(Vec2(0, 0.05));
+	self.setPosition(self.getVelocity() + self.getPosition());
+}
+
 Body trident("  *   *** ^ ^ ^", 5, 3);
 
 EnemyEntity *EnemyFactory::createEnemy(EnemyFactory::EnemyTypes t, const Vec2 position)
@@ -8,7 +14,7 @@ EnemyEntity *EnemyFactory::createEnemy(EnemyFactory::EnemyTypes t, const Vec2 po
 	switch (t)
 	{
 		case BASIC:
-			return (new EnemyEntity(position, basic));
+			return (new EnemyEntity(position, basic, &basicBehavior));
 		case TRIDENT:
 			return (new EnemyEntity(position, trident));
 	}
